bf754c1da4db98ca594e406482f2f481d6f8b648
bera5186 <rbasu611@gmail.com>
2019-11-26 17:39:10 +0530

    Updated

--- Sending-Email-with-Node/app.js
+++ Sending-Email-with-Node/app.js
@@ -1,16 +1,16 @@
-const express    = require('express')
-const app        = express()
-const exphbs     = require('express-handlebars')
-const nodeMailer = require('nodemailer')
-const path       = require('path')
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const nodemailer = require('nodemailer')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.set('view engine','hbs')
-app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 
 // static folder
 app.use('/public', express.static(path.join(__dirname, 'public')))
@@ -19,5 +19,52 @@ app.get('/', (req, res) => {
     res.render('contact')
 })
 
+app.post('/', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
 
+    var transporter = nodemailer.createTransport({
+        // host: "smtp.mailtrap.io",
+        // port: 2525,
+        service: 'gmail',
+        auth: {
+            user: "", // Sender's email address
+            pass: "" // password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let mailOptions = {
+        from: '"Nodemailer Contact" <rbasu611@gmail.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', { msg: 'Email has been sent' });
+    });
+
+
+})
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
